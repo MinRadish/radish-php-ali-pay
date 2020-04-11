60c5bcc9166f4e7242d6ecd6361c49bdfa61da16
Radish <1004622952@qq.com>
2020-04-11 11:39:57 +0800

    修改日志记录方式

--- src/Exception/PayException.php
+++ src/Exception/PayException.php
@@ -23,8 +23,13 @@ class PayException extends \Exception
     {
         $path = $_SERVER['DOCUMENT_ROOT'];
         if (is_dir($path)) {
-            $file = $path . '/AliPay.log';
+                        $path .= DIRECTORY_SEPARATOR . 'AliPay';
+            if (!is_dir($path)) {
+                mkdir($path);
+            }
+            $file = $path . DIRECTORY_SEPARATOR . date('Y-m-d') . '.log';
             $time = date('Y-m-d H:i:s');
+            $this->message = mb_convert_encoding($this->message, 'UTF-8', mb_detect_encoding($this->message));
             file_put_contents($file, $time . PHP_EOL . $this->result() . PHP_EOL . 'message:' . $this->message . PHP_EOL, FILE_APPEND);
         }
     }
